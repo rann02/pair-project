60ef8b8f99487ac130aab5af56ba195d334c8f8c
DESKTOP-LBBL8CS\Fitra <justfitra11@gmail.com>
2023-02-09 13:27:20 +0700

    views

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,6 +3,9 @@ const { Course } = require('../models/index')
 class Controller {
     static home(req, res) {
         Course.findAll()
-            .then(courses => res.render)
+            .then(courses => res.render('home', { courses }))
+            .catch(err => res.send(err))
     }
 }
+
+module.exports = Controller
