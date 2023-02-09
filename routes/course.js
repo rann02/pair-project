ae378f556d98a9c19ccaca0b2f962e75dba8b505
rann02 <sabanarandy98@gmail.com>
2023-02-09 13:32:48 +0700

    tes

--- routes/course.js
+++ routes/course.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Controller = require('../controllers/controller')
 
-router.get('/courses', Controlleer.getCo)
+router.get('/courses',)
 
 
 module.exports()
