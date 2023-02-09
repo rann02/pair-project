96c7e0e9dc482804d589f67479fcda3663f613c4
DESKTOP-LBBL8CS\Fitra <justfitra11@gmail.com>
2023-02-09 16:54:54 +0700

    test

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,39 @@
 const { Course, Category } = require('../models/index')
+const timeFormating = require('../helpers/timeFormating')
+const rupiah = require('../helpers/rupiah')
 
 class Controller {
     static home(req, res) {
         Course.findAll()
-            .then(courses => res.render('home', { courses }))
-            .catch(err => res.send(err))
+            .then(courses => res.render)
+    }
+
+    static getCourse(req, res) {
+        const {filter} = req.query
+        let filterValues
+        // Course.findAll()
+        //     .then(data => {
+        //         res.render('course', { data })
+        //     })
+        //     .catch(err => {
+        //         res.send(err)
+        //     })
+
+        Category.findAll()
+        .then(filterData => {
+            // res.render('course', { filterData })
+            filterValues = filterData
+            return Course.filterCourse(filter)
+        })
+        
+        .then(data => {
+            res.render('course', { data, filterValues, timeFormating, rupiah })
+            // res.send(data)
+        })
+
+        .catch(err => {
+            res.send(err)
+        })
     }
 }
 
